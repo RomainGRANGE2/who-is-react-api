337a42379e393d298b8247ea82b2844049f6f421
Romain Grange <romain.grange@pixlstudio.fr>
2025-02-13 18:22:06 +0100

    wip5

--- src/bdd.js
+++ src/bdd.js
@@ -2,26 +2,12 @@ import { Sequelize } from "@sequelize/core";
 import { MySqlDialect } from "@sequelize/mysql";
 import dotenv from "dotenv";
 
-dotenv.config(); // Charge les variables d'environnement
+dotenv.config();
 
 /**
- * Connexion à la base de données
- */
+
+ Connexion à la base de données*/
 export const sequelize = new Sequelize({
 	dialect: MySqlDialect,
-	database: process.env.DB_NAME || "database_name",
-	user: process.env.DB_USER || "root",  // ✅ Remplacé "user" par "username"
-	password: process.env.DB_PASSWORD || "",
-	host: process.env.DB_HOST || "localhost",
-	port: parseInt(process.env.DB_PORT) || 3306,
-	logging: false, // ✅ Désactive les logs SQL
+	url: 'mysql://root:dSNNltQbHiMayPMqCzMWgrdRugkyFrjX@autorack.proxy.rlwy.net:43614/railway',
 });
-
-(async () => {
-	try {
-		await sequelize.authenticate();
-		console.log("✅ Connexion réussie à Railway MySQL !");
-	} catch (error) {
-		console.error("❌ Erreur de connexion à MySQL :", error);
-	}
-})();
